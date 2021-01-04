a3647b04e7928c8d37b0114a42e19ab78b68c456
eckon <niklas.meyer95@hotmail.de>
2021-01-04 20:00:41 +0100

    Convert tabs to spaces

--- gamemodes/hideandseekduo/gamemode/init.lua
+++ gamemodes/hideandseekduo/gamemode/init.lua
@@ -11,20 +11,20 @@ local net = net
 local CreateConVar = CreateConVar
 
 local roundsuntilrandomize = CreateConVar("has_roundsuntilrandomize",
-				"1",
-				{FCVAR_REPLICATED,FCVAR_SERVER_CAN_EXECUTE,FCVAR_ARCHIVE},
-				"Sets amount of rounds until the teams will be randomized")
+                "1",
+                {FCVAR_REPLICATED,FCVAR_SERVER_CAN_EXECUTE,FCVAR_ARCHIVE},
+                "Sets amount of rounds until the teams will be randomized")
 
 local partnermatching = CreateConVar("has_partnermatching",
-				"0",
-				{FCVAR_REPLICATED,FCVAR_SERVER_CAN_EXECUTE,FCVAR_ARCHIVE},
-				"Makes partner-less players match to a new duo")
+                "0",
+                {FCVAR_REPLICATED,FCVAR_SERVER_CAN_EXECUTE,FCVAR_ARCHIVE},
+                "Makes partner-less players match to a new duo")
 
 if not ConVarExists("has_allowsbonds") then
-	CreateConVar("has_allowsbonds",
-				"0",
-				{FCVAR_REPLICATED,FCVAR_SERVER_CAN_EXECUTE,FCVAR_ARCHIVE},
-				"Allows bond between two players.")
+    CreateConVar("has_allowsbonds",
+                "0",
+                {FCVAR_REPLICATED,FCVAR_SERVER_CAN_EXECUTE,FCVAR_ARCHIVE},
+                "Allows bond between two players.")
 end
 
 -- Pool network names
@@ -39,31 +39,31 @@ util.AddNetworkString("TargetPartnerRemove")
 
 
 hook.Add("HASRoundStarted", "Duo_Round_Started", function()
-	-- round check for randomize
-	activeRoundCounter = activeRoundCounter + 1
-	if activeRoundCounter >= roundsuntilrandomize:GetInt() then
-		activeRoundCounter = 0
-		PARTNERS = CreateRandomPartner(player.GetAll())
-		ACTIVEPARTNERS = PARTNERS
-	else
-		ACTIVEPARTNERS = PARTNERS
-	end
-	--SetInvisibleTbl(player.GetAll())
+    -- round check for randomize
+    activeRoundCounter = activeRoundCounter + 1
+    if activeRoundCounter >= roundsuntilrandomize:GetInt() then
+        activeRoundCounter = 0
+        PARTNERS = CreateRandomPartner(player.GetAll())
+        ACTIVEPARTNERS = PARTNERS
+    else
+        ACTIVEPARTNERS = PARTNERS
+    end
+    --SetInvisibleTbl(player.GetAll())
 end)
 
 
 hook.Add("HASPlayerCaught", "duo_player_caught", function(seeker, entply)
-	removeFromPartner(entply)
+    removeFromPartner(entply)
 end)
 
 hook.Add("PlayerDeath", "duo_player_dead", function(victim, inflictor, attacker)
-	-- TODO: remove any status effects
-	removeFromPartner(victim)
+    -- TODO: remove any status effects
+    removeFromPartner(victim)
 end)
 
 hook.Add("HASGameEnded", "duo_game_ended", function(winner)
-	-- TODO: remove any status effects
-	-- Reset Partners
+    -- TODO: remove any status effects
+    -- Reset Partners
 end)
 
 -- hook.Call("HASPlayerCaught",GAMEMODE,sekr,entply)
--- gamemodes/hideandseekduo/gamemode/sv_duo_abilities.lua
+++ gamemodes/hideandseekduo/gamemode/sv_duo_abilities.lua
@@ -1,93 +1,93 @@
 -- Contains all abilities
 
 local enableabilties = CreateConVar("has_enableabilities",
-				"1",
-				{FCVAR_REPLICATED,FCVAR_SERVER_CAN_EXECUTE,FCVAR_ARCHIVE},
-				"Allows ablities")
+                "1",
+                {FCVAR_REPLICATED,FCVAR_SERVER_CAN_EXECUTE,FCVAR_ARCHIVE},
+                "Allows ablities")
 
 
 function GoInvisible(ply, distance)
-	if distance < 110 then
-		local inv = Color(0, 0, 0, 3)
-		local plyColor = ply:GetColor()
-		for i = 1, #invisibleTbl do
-			if player.GetByID(i) == ply and not invisibleTbl[i] then
-				ply:SetRenderMode(RENDERMODE_TRANSCOLOR)
-				ply:SetColor(inv)
-				ply:EmitSound("AlyxEMP.Charge")
-				ply:SetMaterial( "sprites/heatwave" )
-				invisibleTbl[i] = true
-				ply:PrintMessage(HUD_PRINTTALK, "Du bist für 15 Sekunden unsichtbar")
-				timer.Simple(15, function()
-					ply:SetColor(plyColor)
-					--ply:EmitSound("AlyxEMP.Charge")
-					ply:SetMaterial("models/glass")
-					ply:PrintMessage(HUD_PRINTTALK, "Du bist wieder sichtbar")
-				end)
-			end
-			if player.GetByID(i) == partner and not invisibleTbl[i] then
-				partner:SetRenderMode(RENDERMODE_TRANSCOLOR)
-				partner:SetColor(inv)
-				partner:SetMaterial( "sprites/heatwave" )
-				invisibleTbl[i] = true
-				partner:PrintMessage(HUD_PRINTTALK, "Du bist für 15 Sekunden unsichtbar")
-				timer.Simple(15, function()
-					partner:SetColor(plyColor)
-					--partner:EmitSound("AlyxEMP.Charge")
-					partner:PrintMessage(HUD_PRINTTALK, "Du bist wieder sichtbar")
-					partner:SetMaterial("models/glass")
-				end)
-			end
-		end
-	end
+    if distance < 110 then
+        local inv = Color(0, 0, 0, 3)
+        local plyColor = ply:GetColor()
+        for i = 1, #invisibleTbl do
+            if player.GetByID(i) == ply and not invisibleTbl[i] then
+                ply:SetRenderMode(RENDERMODE_TRANSCOLOR)
+                ply:SetColor(inv)
+                ply:EmitSound("AlyxEMP.Charge")
+                ply:SetMaterial( "sprites/heatwave" )
+                invisibleTbl[i] = true
+                ply:PrintMessage(HUD_PRINTTALK, "Du bist für 15 Sekunden unsichtbar")
+                timer.Simple(15, function()
+                    ply:SetColor(plyColor)
+                    --ply:EmitSound("AlyxEMP.Charge")
+                    ply:SetMaterial("models/glass")
+                    ply:PrintMessage(HUD_PRINTTALK, "Du bist wieder sichtbar")
+                end)
+            end
+            if player.GetByID(i) == partner and not invisibleTbl[i] then
+                partner:SetRenderMode(RENDERMODE_TRANSCOLOR)
+                partner:SetColor(inv)
+                partner:SetMaterial( "sprites/heatwave" )
+                invisibleTbl[i] = true
+                partner:PrintMessage(HUD_PRINTTALK, "Du bist für 15 Sekunden unsichtbar")
+                timer.Simple(15, function()
+                    partner:SetColor(plyColor)
+                    --partner:EmitSound("AlyxEMP.Charge")
+                    partner:PrintMessage(HUD_PRINTTALK, "Du bist wieder sichtbar")
+                    partner:SetMaterial("models/glass")
+                end)
+            end
+        end
+    end
 end
 
 function GoProp(ply, distance)
-	ply:PrintMessage(HUD_PRINTTALK, "Foo")
+    ply:PrintMessage(HUD_PRINTTALK, "Foo")
 end
 
 function SetInvisibleTbl(Players)
-	for i = 1, #Players do
-		invisibleTbl[i] = false
-	end
+    for i = 1, #Players do
+        invisibleTbl[i] = false
+    end
 end
 
 function GetInvisibleTblValue(ply)
-	for i = 1, #invisibleTbl do
+    for i = 1, #invisibleTbl do
 
-	end
+    end
 end
 
 function ExecuteAbility(ply, distance)
-	local ability = 0 -- Invisibility (TODO: Make multiple of these)
-	if ability == 0 then
-		GoInvisible(ply, distance)
-	end
+    local ability = 0 -- Invisibility (TODO: Make multiple of these)
+    if ability == 0 then
+        GoInvisible(ply, distance)
+    end
 end
 
 hook.Add( "KeyPress", "keypressForAction", function( ply, key )
-	if has_enableabilities == 0 then return end -- disable abilities
+    if has_enableabilities == 0 then return end -- disable abilities
 
-	-- use a nice switch-case
-	local switch = {
-		[IN_RELOAD] = function()
-			GoProp(ply)
-		end,
-		[IN_USE] = function()
-			-- Distance Calc
-			local playerPos = ply:GetPos()
-			local partner = GetPartner(ply, ACTIVEPARTNERS)
-			if not partner then return end
+    -- use a nice switch-case
+    local switch = {
+        [IN_RELOAD] = function()
+            GoProp(ply)
+        end,
+        [IN_USE] = function()
+            -- Distance Calc
+            local playerPos = ply:GetPos()
+            local partner = GetPartner(ply, ACTIVEPARTNERS)
+            if not partner then return end
 
-			local partnerPos = partner:GetPos()
-			local distance = math.Distance( playerPos.x, playerPos.y, partnerPos.x, partnerPos.y )
+            local partnerPos = partner:GetPos()
+            local distance = math.Distance( playerPos.x, playerPos.y, partnerPos.x, partnerPos.y )
 
-			ExecuteAbility(ply, distance)
-		end,
-	}
+            ExecuteAbility(ply, distance)
+        end,
+    }
 
-	local action = switch[key]
-	if(action) then
-		action()
-	end
+    local action = switch[key]
+    if(action) then
+        action()
+    end
 end)
